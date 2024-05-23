3a9a3c4de310c7a62dea78befe99f115fb8e76ee
ayan878 <ayanraza197@gmail.com>
2024-05-23 17:33:59 +0530

    username component

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,27 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import avatar from "../assets/profile.png";
+import toast, { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+
 import styles from "../styles/Username.module.css";
+import { usernameValidate } from "../helper/validate";
 
 export default function Username() {
+  const formik = useFormik({
+    initialValues: {
+      username: "Example123",
+    },
+    validate: usernameValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      toast.success("Form submitted successfully!");
+      console.log(values);
+    },
+  });
+
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center overflow-hidden py-10">
-      <div className="flex justify-center items-center h-full w-full">
+    <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
+
+      <div className="flex justify-center items-center h-screen">
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
             <h4 className="text-3xl font-bold">Hello Again!</h4>
-            <span className="py-4 text-sm w-2/3 text-center text-gray-500">
+            <span className="py-2 text-sm w-2/3 text-center text-gray-500">
               Explore More by connecting with us.
             </span>
           </div>
 
-          <form className="py-1">
+          <form className="py-1" onSubmit={formik.handleSubmit}>
             <div className="profile flex justify-center py-4">
               <img src={avatar} className={styles.profile_img} alt="avatar" />
             </div>
 
-            <div className="textbox flex flex-col items-center gap-6">
+            <div className="textbox flex flex-col items-center gap-4">
               <input
+                {...formik.getFieldProps("username")}
                 className={styles.textbox}
                 type="text"
                 placeholder="Username"
@@ -31,8 +51,8 @@ export default function Username() {
               </button>
             </div>
 
-            <div className="text-center py-4">
-              <span className="text-gray-500">
+            <div className="text-center">
+              <span className="text-gray-500 text-xs">
                 Not a Member{" "}
                 <Link className="text-red-500" to="/register">
                   Register Now
