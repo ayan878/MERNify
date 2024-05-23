99fc1d8fa560d645bce85bc278b76cc4f335da17
ayan878 <ayanraza197@gmail.com>
2024-05-23 17:33:39 +0530

    reset component

--- client/src/components/Reset.js
+++ client/src/components/Reset.js
@@ -1,9 +1,59 @@
-function Reset() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import React from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import styles from "../styles/Username.module.css";
+import { resetPasswordValidation } from "../helper/validate";
 
-export default Reset
+
+export default function Username() {
+  const formik = useFormik({
+    initialValues: {
+    password:"",
+    confirm_pwd:"",
+    },
+    validate: resetPasswordValidation,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      toast.success("Password reset successfully!");
+      console.log(values);
+    },
+  });
+
+  return (
+    <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
+
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass} >
+          <div className="title flex flex-col items-center py-8">
+            <h4 className="text-3xl font-bold">Reset</h4>
+            <span className="py-2 text-sm w-2/3 text-center text-gray-500">
+              Explore More by connecting with us.
+            </span>
+          </div>
+
+          <form className="py-1" onSubmit={formik.handleSubmit}>
+            <div className="textbox flex flex-col items-center gap-4">
+              <input
+                {...formik.getFieldProps("password")}
+                className={styles.textbox}
+                type="text"
+                placeholder="New Password"
+              />
+              <input
+                {...formik.getFieldProps("confirm_pwd")}
+                className={styles.textbox}
+                type="text"
+                placeholder="Repeat Password"
+              />
+              <button className={styles.btn} type="submit">
+                Reset
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
