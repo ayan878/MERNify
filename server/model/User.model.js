fdd4b7975a18916694d0d7d186bc0b63452cd835
ayan878 <ayanraza197@gmail.com>
2024-05-25 14:59:08 +0530

    user model

---
+++ server/model/User.model.js
@@ -0,0 +1,37 @@
+import mongoose, { model } from "mongoose";
+
+export const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a unique username"],
+    unique: [true, "Username exist"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    unique: false,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a email"],
+    unique: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  mobile: {
+    type: Number,
+  },
+  address: {
+    type: String,
+  },
+  profile: {
+    type: String,
+  },
+});
+// if we already have User model inside mongoDB database then use that n=model and return that user model and we have to specify with  plural  otherwise we get erorr message
+export default mongoose.model.Users || mongoose.model("User", UserSchema);
+// existing model ||  new model
