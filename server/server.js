8229ebf3bac071c35392d5aa33e0aa440b1b70d4
ayan878 <ayanraza197@gmail.com>
2024-06-11 00:21:20 +0530

    server

--- server/server.js
+++ server/server.js
@@ -1,51 +1,7 @@
-// import express from "express";
-// import cors from "cors";
-// import morgan from "morgan";
-// import connect from "./database/conn.js";
-// import router from "./router/route.js";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const app = express();
-
-
-// // Middleware
-// app.use(express.json());
-// app.use(cors());
-// app.use(morgan("tiny")); // it log the http format to console 
-// app.disable("x-powered-by"); // less hackers know about our stack
-
-// // HTTP GET Request
-// app.get("/", (req, res) => {
-//   res.status(201).json("Home GET Request");
-// });
-
-// // API routes
-// app.use("/api", router);
-
-// const port = 8000;
-
-// // Start server when connected to database
-// connect()
-//   .then(() => {
-//     try {
-//       app.listen(port, () => {
-//         console.log(`Server connected to http://localhost:${port}`);
-//       });
-//     } catch (error) {
-//       console.log("Can't connect to the server");
-//     }
-//   })
-//   .catch((error) => {
-//     console.log("Invalid database connection...!");
-//   });
-
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import mongoose from "mongoose";
-import connect from "./database/conn.js";
 import router from "./router/route.js";
 import dotenv from "dotenv";
 
@@ -57,7 +13,6 @@ mongoose
   .connect(process.env.MONGODB_CONNECT_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // Remove useFindAndModify: false from here
   })
   .then(() => console.log("Connected to database"))
   .catch((error) => console.error("Database connection error:", error));
